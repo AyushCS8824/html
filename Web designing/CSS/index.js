f5c3700980f781e9493c0ba104d42bd960ee7b12
AyushCS8824 <ayushsoni42543@gmail.com>
2024-01-18 20:20:05 +0530

    hihi

---
+++ Web designing/CSS/index.js
@@ -0,0 +1,260 @@
+// var s = 5
+// var s = 10
+// console.log(s)
+
+
+
+
+
+// let value = 100
+// let to = value ==100
+// console.log(to)
+
+
+
+
+
+
+
+// var firstName='Ram'
+// var lastName='Sita'
+// console.log(firstName+" "+lastName)
+// console.log('${Ram}${Sita}')
+
+
+
+
+
+
+// let user=null
+// console.log(user)
+
+
+
+// if(0){
+//     console.log("Toy")
+// }
+// else{
+//     console.log("Army")
+// }
+
+
+
+
+
+// const add=()=>{
+//     console.log("hiiiiiii")
+// }
+// add()
+
+
+
+
+
+// function joy() {
+//     console.log("hello")
+    
+// }
+// joy()
+// let arr = [1,2,3,4,5,6];
+// let a = 0;
+// for(let i of arr){
+//     a = a+i;
+// }
+// console.log(a)
+
+
+
+
+// Object
+// let students ={ 
+//     name:'abc',
+//     age:22,
+//     isPass:true
+// }
+// students.name='xyz'
+// console.log(students)
+    
+
+
+
+
+// let obj ={
+//     user:'modi',
+//     lastName:'ji',
+//     sum:function(){
+//         console.log(this.user+this.lastName)
+
+//     }
+// }
+//    obj.sum();
+
+
+
+
+
+
+
+
+// let arr=[{id:1,name:"Ayush"}]
+// arr.map ((k)=>{
+//     console.log(k.id);
+
+// })
+// console.log()
+
+
+
+
+
+
+// let arr=[1,2,3,4,56]
+// console.log(arr[0].id)
+// arr.push(10)
+// arr.pop()
+// arr.unshift(0)
+// console.log(arr)    
+
+
+
+
+
+// var arr1 = [1,2,3,4,5,'string','hello',true,false]             // For Find String Value
+// function getstring(arr1) {
+//     let res = []
+//     for(let item of arr1){
+//         if (typeof item =='string')
+//         {
+//             res.push(item)
+
+//         }
+//     }
+//     return res
+    
+// }
+// console.log(getstring(arr1))
+
+
+
+
+// var arr1 = [1,2,3,4,5,'string','hello',true,false]                     // For Find Number Value
+// function getNumber(arr1) {
+//     let res = []
+//     for(let item of arr1){
+//         if (typeof item =='number')
+//         {
+//             res.push(item)
+
+//         }
+//     }
+//     return res
+    
+// }
+// console.log(getNumber(arr1))
+
+
+
+
+// var arr1 = [1,2,3,4,5,'string','hello',true,false]                     // For Find Boolean Value
+// function getBool(arr1) {
+//     let res = []
+//     for(let item of arr1){
+//         if (typeof item =='boolean')
+//         {
+//             res.push(item)
+
+//         }
+//     }
+//     return res
+    
+// }
+// console.log(getBool(arr1))
+
+
+
+
+// var arr1 = [1,2,3,4,5,'string','hello',true,false]              // For Find All Types Of Data Value
+// function getstring(item) {
+//     return typeof item ==='string'
+    
+// }
+// function getNumber(item) {
+//     return typeof item ==='number'
+    
+// }
+// function getboolean(item) {
+//     return typeof item ==='boolean'
+    
+// }
+// function get(arr1,fn) {
+//     let res = []
+//     for(let item of arr1){
+//         if(fn(item)){
+//             res.push(item)
+//         }
+//     }
+//     return res
+    
+// }
+// console.log(get(arr1,getstring))
+// console.log(get(arr1,getNumber))
+// console.log(get(arr1,getboolean))
+
+
+
+
+
+
+// function outer() {                                    // Execution Question
+//     let user = 'yhi aayega'
+//     function inner() {
+//         console.log(user)
+        
+//     }
+//     // inner();
+
+//     return inner
+    
+// }
+// let a =         outer();
+// a();
+
+
+
+
+
+
+
+  
+// console.log(typeof "Hello,World!");
+
+
+
+
+// Topic Name Closure.................
+
+
+
+
+
+
+
+
+
+
+// let obj = {
+//     id:1,
+//     user:'djkcmnidcjd',
+//     print:function () {
+//         console.log(this.user)
+        
+//     }
+// }
+// // obj.print();
+// console.log(obj.toString())
+
+
+
+
+
+
